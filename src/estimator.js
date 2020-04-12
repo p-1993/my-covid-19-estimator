c021873b3c08b1c43566efbefdcdedfcb13423bf
p-1993 <okeropatrick@gmail.com>
2020-04-12 13:38:47 +0300

    build my estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    const { reportedCases, periodType, timeToElapse } = data;
+    const currentlyInfected = reportedCases * 10;
+    let timeToElapse;
+    switch (periodType.lowertoLowerCase) {
+        case 'months':
+            elapse = math.trunc(timeToElapse / 3) * 30
+            break;
+        case 'weeks':
+            elapse = math.trunc(timeToElapse / 3) * 7
+            break;
+        default:
+            elapse = math.trunc(timeToElapse / 3)
+            break
+    }
+    const infectionByRequestedTime = currentlyInfected * (2 ** elapse);
+    return {
+        data: {},
+        impact: {},
+        severeImpact: {},
+        currentlyInfected,
+        infectionByRequestedTime,
+    };
+};
 
 export default covid19ImpactEstimator;
